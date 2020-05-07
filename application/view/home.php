e417a7c9c4aadac14c4bf21e1aeb6960996cbbdd
Pitsillides Charalambos <harryspitsillides@hotmail.com>
2020-05-07 15:53:58 +0100

    Update html and css added photorealistic images and MVC from labs

---
+++ application/view/home.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="node_modules/bootswatch/dist/darkly/bootstrap.min.css"/>
+    <link rel="stylesheet" href="node_modules/x3dom/x3dom.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                   aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<main class="mt-5 pt-5">
+    <div class="container">
+        <div id="mainContent">
+            <!--Section: Jumbotron-->
+            <section class="card" style="background-image: url(application/assets/images/hero-background.jpg)">
+
+                <!-- Content -->
+                <div class="card-body text-white text-center py-5 px-5 my-5">
+
+                    <h1 class="mb-4">
+                        <strong>Welcome to the Gun Museum!</strong>
+                    </h1>
+                    <p>
+                        <strong>Scroll further down to see our selection individually or click here to go to our showcase!</strong>
+                    </p>
+                    <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/" class="btn btn-outline-white btn-lg">Start
+                        free tutorial
+                        <i class="fas fa-graduation-cap ml-2"></i>
+                    </a>
+
+                </div>
+                <!-- Content -->
+            </section>
+            <!--Section: Jumbotron-->
+
+            <hr class="my-5">
+
+            <!--Section: Magazine v.1-->
+            <section class="wow fadeIn">
+
+                <!--Section heading-->
+                <h2 class="h1 text-center my-5 font-weight-bold">Our collection</h2>
+
+                <!--Grid row-->
+                <div class="row text-left">
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12">
+                        <!--Image-->
+                        <div class="view overlay rounded z-depth-1-half mb-3">
+                            <img src="application/assets/images/scar_demo_pic.jpg" class="img-fluid" alt="Sample post image">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <!--Excerpt-->
+                        <h3>
+                            <a>
+                                <strong>This is title of the news</strong>
+                            </a>
+                        </h3>
+                        <p> Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
+                            placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+                        </p>
+                        <hr>
+                    </div>
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12">
+                        <!--Image-->
+                        <div class="view overlay rounded z-depth-1-half mb-3">
+                            <img src="application/assets/images/desertEaglePhotorealistic.jpg" class="img-fluid" alt="Sample post image">
+                            <a>
+                                <div class="mask rgba-white-slight"></div>
+                            </a>
+                        </div>
+                        <div class="news-data">
+                            <a href="" class="light-blue-text">
+                                <h6>
+                                    <i class="fas fa-plane"></i>
+                                    <strong> Travels</strong>
+                                </h6>
+                            </a>
+                            <p>
+                                <strong>
+                                    <i class="far fa-clock"></i> 20/08/2018</strong>
+                            </p>
+                        </div>
+                        <h3>
+                            <a>
+                                <strong>This is title of the news</strong>
+                            </a>
+                        </h3>
+                        <p> Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime
+                            placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+                        </p>
+                        <!--/Featured news-->
+                        <hr>
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+
+            </section>
+            <!--/Section: Magazine v.1-->
+
+        </div>
+    </div>
+</main>
+<!--Main layout-->
+
+<script src="./node_modules/jquery/dist/jquery.min.js"></script>
+<script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="./node_modules/x3dom/x3dom.js"></script>
+<script src="./dist/bundle.js"></script>
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
